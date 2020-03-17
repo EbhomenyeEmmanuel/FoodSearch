033fedbefac0ccefb36685ee7ae0bddbd35ee21e
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-03-17 01:41:50 +0100

    I added Path to the parameters.

--- app/src/main/java/com/esq/foodsearch/FatSecretApiInterface.java
+++ app/src/main/java/com/esq/foodsearch/FatSecretApiInterface.java
@@ -3,10 +3,8 @@ package com.esq.foodsearch;
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
@@ -16,17 +14,19 @@ import retrofit2.http.Query;
 public interface FatSecretApiInterface {
     // API's endpoints
     @FormUrlEncoded
-    @POST("https://oauth.fatsecret.com/connect/token")
+    @POST("https://oauth.fatsecret.com/connect/token")//Access token URL
     Call<UserTokenModel> login(@Field("grant_type") String grantType,
                                @Field("client_id") String username,
                                @Field("client_secret") String password,
                                @Field("scope") String scope);
-
-    @POST("method=foods.search")//&search_expression={search_expression}&format=json
-    Call<List<SearchListResponseModel>> getResultsList(@Header("Authorization") String authToken,
-                                                       @Query("format") String format,
-                                                       @Query("search_expression") String searchKey);
-    // SearchListResponseModel is POJO class to get the data from API, In above method we use List<SearchListResponseModel>
+    //@FormUrlEncoded
+    @Headers({ "Content-Type: application/json;charset=UTF-8"})
+    @POST("/{method}/{search_expression}/{format}")
+    Call<List<FoodModelClass>> getResultsList(@Header("Authorization") String authToken,
+                                        @Path("method") String method,
+                                        @Path("search_expression") String searchKey,
+                                        @Path("format") String format);
+    // FoodModel is POJO class to get the data from API, In above method we use Call<FoodModelClass>
     // because the data in our API is starting from JSONArray
     // and callback is used to get the response from api and it will set it in our POJO class
 }
