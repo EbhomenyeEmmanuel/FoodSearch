91686d4489ea9a0b18021c00f8f7e4a339492530
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-03-21 02:14:08 +0100

    Welcome UI Activity(Updated code).

--- app/src/main/java/com/esq/foodsearch/LoginActivity.java
+++ app/src/main/java/com/esq/foodsearch/LoginActivity.java
@@ -1,16 +1,15 @@
 package com.esq.foodsearch;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import retrofit2.Call;
@@ -46,9 +45,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         progressDialog.setCancelable(false); // set cancelable to false
         progressDialog.setMessage("Please Wait... Token is generated"); // set message
         progressDialog.show(); // show progress dialog
-        String username = "15763421aecc458294ee304cd7cab984";//Client ID gotten from developers account
-        String password = "5ed557eaca4b4307a7cd21dc506a088f";//Client Secret gotten from developers account
-        //String[] clientCredentials= {username, password};
+        final String username = "15763421aecc458294ee304cd7cab984";//Client ID gotten from developers account
+        final String password = "5ed557eaca4b4307a7cd21dc506a088f";//Client Secret gotten from developers account
+
         // Create a user model
         final LoginModel loginModel = new LoginModel(username, password);
         //Call the retrofit Interface built.
@@ -81,5 +80,26 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         });
     }
 
+    //Create an exit dialog
+    @Override
+    public void onBackPressed() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+        builder.setMessage("Are you sure you want to exit?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("Yes!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 
 }
