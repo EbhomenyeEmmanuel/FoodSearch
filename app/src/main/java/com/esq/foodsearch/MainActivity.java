2852768eafa74fbcd145ef1d926a2e8c7c224553
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-03-21 02:20:32 +0100

    Main Activity. The intent gotten from LoginActivity is stored here as a token to make API calls. The value gotten from editText is stored as a searchKey i.e the food that is searched for.

--- app/src/main/java/com/esq/foodsearch/MainActivity.java
+++ app/src/main/java/com/esq/foodsearch/MainActivity.java
@@ -1,24 +1,21 @@
 package com.esq.foodsearch;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -27,38 +24,14 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     public static final String TOKEN = "token";
     String TAG = "MainActivity.this";
-    int tokenExpirationChecker;
     int tokenTimeLeft; //From intent
-    List<SearchListResponseModel> searchListResponseData ;//List of results found
+    ArrayList searchListResponseData ;//List of results found
     ProgressDialog progressDialog;
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        //handlerMethod();
-    }
-
-    //Check if token time has expired
-    public void handlerMethod(){
-        int SPLASH_TIME = 1000;
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (tokenExpirationChecker == tokenTimeLeft) {
-                    Toast.makeText(MainActivity.this, "Token Expired... Requesting a new one", Toast.LENGTH_SHORT).show();
-                    new LoginActivity().loginToSearchFood();
-                }else {
-                    ++tokenExpirationChecker;
-                }
-                Log.i("MainActivity", "Handler method is incrementing value");
-            }
-        }, SPLASH_TIME);
-    }
+    String searchKey;//String Value of food to search for
 
     @Override
     protected void onStop() {
         super.onStop();
-        progressDialog.dismiss(); //dismiss progress dialog
         finish();
     }
 
@@ -67,7 +40,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        getUserListData(); // call a method in which we have implement our GET type web API
+        popUpDialog();
+    }
+
+    private void popUpDialog() {
+        final EditText taskEditText = new EditText(this);
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Search Key")
+                .setMessage("Add food to search for :)")
+                .setView(taskEditText)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        searchKey = String.valueOf(taskEditText.getText()); //Make the value of the editText equal to the searchKey
+                        Log.d(TAG, "Value of editText is " + searchKey);
+                        if (searchKey != null) {
+                           getUserListData();// call a method in which we have implement our GET type web API
+                        }else{
+                            Toast.makeText(MainActivity.this, "Search Key is empty", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .setCancelable(false)
+                .create();
+        dialog.show();
     }
 
     //Use token gotten from intent to set upRecyclerView
@@ -79,34 +76,48 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String token = intent.getStringExtra(TOKEN);
         tokenTimeLeft = intent.getIntExtra("tokenTimeLeft", 0);
-        Toast.makeText(this, "Token time left is " + tokenTimeLeft + " \n Token is " + token , Toast.LENGTH_LONG).show();
         Log.d(TAG, " \n Token is " + token);
-        String searchKey = "toast";//Food to search for
-
-        Api.getClient().getResultsList(token, "json", searchKey).enqueue(new Callback<List<SearchListResponseModel>>() {
+        Api.getClient().getResultsList("Bearer " +token,"foods.search", searchKey, "json")
+                .enqueue(new Callback<FoodModelClass>(){
             @Override
-            public void onResponse(Call<List<SearchListResponseModel>> call, Response<List<SearchListResponseModel>> response) {
+            public void onResponse(Call<FoodModelClass> call, Response<FoodModelClass> response) {
                 progressDialog.dismiss(); //dismiss progress dialog
                 if (response.isSuccessful()) {
-                    Log.d("responseGET ", response.body().get(0).getName());
-                    searchListResponseData = response.body();//Add all the results in a list
-                    setDataInRecyclerView();
+                   Log.d(TAG,"Login was successful");
+                    if (response.body() == null || response.body().getFoods() == null || response.body().getFoods().getFood() == null) {
+                        Log.d(TAG,"Response Body is null");
+                    }else{
+                        Log.d(TAG,"Response Body is not null");
+                        searchListResponseData = response.body().getFoods().getFood();//Add all the results in a list
+                        if (searchListResponseData == null) {
+                            Log.d(TAG,"Successful Response: searchListResponseData is null" );
+                        }else{
+                            Log.d(TAG,"Successful Response: searchListResponseData is not null" );
+                        }
+                        setDataInRecyclerView();
+                    }
+
                 }else{
-                    Log.d("Response errorBody", String.valueOf(response.errorBody()));
-                    Toast.makeText(MainActivity.this, response.errorBody().toString(), Toast.LENGTH_SHORT).show();
+                    Log.d(TAG,"OnResponse () -> Error: "+ response.code());
+                    try {
+                        Log.d(TAG,"Error: "+ response.errorBody().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(MainActivity.this, "Error: "+ response.code(), Toast.LENGTH_LONG).show();
                 }
             }
 
             @Override
-            public void onFailure(Call<List<SearchListResponseModel>> call, Throwable t) {
+            public void onFailure(Call<FoodModelClass> call, Throwable t) {
                 // if error occurs in network transaction then we can get the error in this method.
                 Toast.makeText(MainActivity.this, t.toString(), Toast.LENGTH_LONG).show();
-                //progressDialog.dismiss(); //dismiss progress dialog
             }
         });
     }
 
     private void setDataInRecyclerView(){
+        Log.d(TAG,"setDataInRecyclerView(): Data in recyclerView is set up");
         // set a LinearLayoutManager with default vertical orientation
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(linearLayoutManager);
