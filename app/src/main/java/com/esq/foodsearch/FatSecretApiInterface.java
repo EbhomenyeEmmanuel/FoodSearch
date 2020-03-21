96f47b555ccb9711ad5f2deeed7288a096d95370
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-03-21 02:26:57 +0100

    Interface implementation for the REST API used.

--- app/src/main/java/com/esq/foodsearch/FatSecretApiInterface.java
+++ app/src/main/java/com/esq/foodsearch/FatSecretApiInterface.java
@@ -1,14 +1,12 @@
 package com.esq.foodsearch;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface FatSecretApiInterface {
@@ -19,14 +17,14 @@ public interface FatSecretApiInterface {
                                @Field("client_id") String username,
                                @Field("client_secret") String password,
                                @Field("scope") String scope);
-    //@FormUrlEncoded
+
     @Headers({ "Content-Type: application/json;charset=UTF-8"})
-    @POST("/{method}/{search_expression}/{format}")
-    Call<List<FoodModelClass>> getResultsList(@Header("Authorization") String authToken,
-                                        @Path("method") String method,
-                                        @Path("search_expression") String searchKey,
-                                        @Path("format") String format);
-    // FoodModel is POJO class to get the data from API, In above method we use Call<FoodModelClass>
+    @GET("https://platform.fatsecret.com/rest/server.api?")
+    Call<FoodModelClass> getResultsList(@Header("Authorization") String authToken,
+                                        @Query("method") String method,
+                                        @Query("search_expression") String searchKey,
+                                        @Query("format") String format);
+    // FoodModel is POJO class to get the data from API, In above method I used Call<FoodModelClass>
     // because the data in our API is starting from JSONArray
     // and callback is used to get the response from api and it will set it in our POJO class
 }
