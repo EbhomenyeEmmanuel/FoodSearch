7c979480cc2a30dfee3594a53c853cd570d65097
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-03-21 02:21:30 +0100

    Adapter Class for recyclerView.

--- app/src/main/java/com/esq/foodsearch/UserAdapter.java
+++ app/src/main/java/com/esq/foodsearch/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.esq.foodsearch;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,51 +11,56 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UsersViewHolder>{
     Context context;
-    List<SearchListResponseModel> searchListResponseModelData;
-
+    ArrayList<Food> foodData;
+    String TAG = "UserAdapter.this";
 
     @NonNull
     @Override
     public UserAdapter.UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
         View view = LayoutInflater.from(context).inflate(R.layout.user_list_items, null);
         return new UsersViewHolder(view);
         }
 
-    public UserAdapter(Context context, List<SearchListResponseModel> searchListResponseModelData) {
-        this.searchListResponseModelData = searchListResponseModelData;
+    public UserAdapter(Context context, ArrayList<Food> foodData) {
+        this.foodData = foodData;
         this.context = context;
+        if (foodData == null) {
+            Log.d(TAG,"Constuctor: FoodData is null" );
+        }
     }
 
     @Override
     public void onBindViewHolder(@NonNull UserAdapter.UsersViewHolder holder, final int position) {
         // set the data
-        holder.name.setText("Name: " + searchListResponseModelData.get(position).getName());
-        holder.type.setText("Type " + searchListResponseModelData.get(position).getType());
-        holder.brandName.setText("Brand Name: " + searchListResponseModelData.get(position).getBrandName());
-        // implement setONCLickListtener on itemView
+        holder.name.setText("Name: " + foodData.get(position).getFoodName());
+        holder.type.setText("Type: " + foodData.get(position).getFoodType());
+        holder.brandName.setText("Brand Name: " + foodData.get(position).getBrandName());
+        //implement setOnCLickListener on itemView
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // display a toast with user name
-                Toast.makeText(context, searchListResponseModelData.get(position).getName(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, foodData.get(position).getFoodName(), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     @Override
     public int getItemCount() {
-       return searchListResponseModelData.size(); // size of the list items;
+        if (foodData == null) {
+            Log.d(TAG,"getItemCount: Item is null" );
+        }else{
+            Log.d(TAG,"getItemCount: Item is " + foodData.size() );
+        }
+       return foodData != null ?foodData.size(): 0; // size of the list items;
     }
 
     class UsersViewHolder extends RecyclerView.ViewHolder {
         // init the item view's
         TextView name, type, brandName;
-
         public UsersViewHolder(View itemView) {
             super(itemView);
             // get the reference of item view's
