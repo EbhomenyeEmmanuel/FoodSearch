7871c72d55000b76964db0307ff7bf129067f024
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-07-22 14:41:36 +0100

    Added a constants and utils methods.

---
+++ app/src/main/java/com/esq/foodsearch/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.esq.foodsearch.utils
+
+class Constants {
+    companion object {
+        const val CREDENTIALS_RESPONSE = "credentials"
+        const val CREDENTIALS_INFO = "user"
+    }
+}
---
+++ app/src/main/java/com/esq/foodsearch/utils/Utils.kt
@@ -0,0 +1,11 @@
+package com.esq.foodsearch.utils
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.shortToast(msg: String){
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
+fun Context.longToast(msg: String){
+    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+}
